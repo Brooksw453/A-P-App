ca3a57679f42b534d0fa60668f978532391d9ee5
Brooks Winchell <bwinchell@qcc.mass.edu>
2023-08-04 17:44:45 -0400

    Branch 1.1.5
    I added a custom script to turn the bone item menu on if it is not in the socket. However, I need to also check if it is held before turning it off. Not sure how to check if it is held

---
+++ Assets/Scripts/YourScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class YourScript : MonoBehaviour
+{
+    [SerializeField]
+    public GameObject yourGameObject;
+    
+    // Rest of your script
+}
