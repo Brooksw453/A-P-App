bb31440bc34d60b4322430812c5c564193e6a8a7
Brooks Winchell <bwinchell@qcc.mass.edu>
2023-09-29 21:24:27 -0400

    Revisions 1.1
    After reverting a failed attempt to update handtracking, I added the face and mouth assets at $10 and $40

---
+++ Assets/Scripts/TimeDisplay.cs
@@ -0,0 +1,32 @@
+using System;
+using TMPro;
+using UnityEngine;
+
+public class TimeDisplay : MonoBehaviour
+{
+    public TextMeshProUGUI currentTimeText; // Drag your TextMeshPro object for current time here in the inspector
+    public TextMeshProUGUI timerText;       // Drag your TextMeshPro object for timer here in the inspector
+
+    private DateTime startTime;
+
+    private void Start()
+    {
+        // Store the time when the app started
+        startTime = DateTime.UtcNow;
+
+        // Update the display every second
+        InvokeRepeating("UpdateDisplay", 0, 1.0f);
+    }
+
+private void UpdateDisplay()
+{
+    // Get current time in EST
+    DateTime estTime = DateTime.UtcNow.AddHours(-5); // UTC to EST conversion (this doesn't account for daylight saving time)
+    currentTimeText.text = estTime.ToString("hh:mm:ss tt"); // 12-hour format with AM/PM
+
+    // Calculate elapsed time
+    TimeSpan elapsedTime = DateTime.UtcNow - startTime;
+    timerText.text = string.Format("{0:00}:{1:00}:{2:00}", elapsedTime.Hours, elapsedTime.Minutes, elapsedTime.Seconds);
+}
+
+}
